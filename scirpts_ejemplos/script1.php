f4b9f425458531ce088fc0ad99be2662a43a3940
Mitnick021 <sergio.pinta2016123@gmail.com>
2019-03-15 12:00:15 -0500

    primeros scripts de php

---
+++ scirpts_ejemplos/script1.php
@@ -0,0 +1,16 @@
+
+<center>progrmacion avanzada</center>
+<?php
+   $nombre="peter";
+   $apelllido="watson";
+   $edad=30;
+   $salario=800;
+
+   echo "<center>TODO COPAS</center>";
+   echo "Nombre: ". $nombre ."<br>";
+   echo "Apellido: ". $apelllido ."<br>";
+   echo "edad: ". $edad;
+   echo "<br>Nombre: ". $salario;
+
+?>
+<center>Fin del programa </center>
---
+++ scirpts_ejemplos/script2.php
@@ -0,0 +1,19 @@
+<center>calculadora</center> 
+<?php
+//esta es mi primera calculadora
+     $num1=10;
+     $num2=5;
+
+     $suma=$num1+$num2;
+     $resta=$num1-$num2;
+     $mult=$num1*$num2;
+     $div=$num1/$num2;
+
+     echo "la suma es: ". $suma;
+     echo "<br>la resta es: ". $resta;
+     echo "<br>la multiplicacion es: ". $mult;
+     echo "<br>la division es: ". $div;
+
+
+
+ ?>
